b08f48aaefe213c17252e2301902efaf5a416c22
shadowrappro <sobirovumidjonobidjonovich@gmail.com>
2024-10-04 00:12:41 +0500

    kodni qo'shdim

--- fn30vazifa3.py
+++ fn30vazifa3.py
@@ -1,1 +1,12 @@
 # gitga ulash muvaffaqiyatli amalga oshirildi
+
+foydalanuvchi_string = input("Iltimos, biror matn kiriting: ")
+
+try:
+    if not foydalanuvchi_string.isalpha():
+        raise ValueError("Kiritilgan stringda faqat harflar bo'lishi kerak.")
+    
+    print("Kiritilgan string to'g'ri: faqat harflardan iborat.")
+
+except ValueError as e:
+    print(f"xato: {e}")
